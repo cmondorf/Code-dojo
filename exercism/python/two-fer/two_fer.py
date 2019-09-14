7147716191ce424c3111eb9eadd7eb4768193538
Christian Mondorf <christian@mondorf.dk>
2019-09-14 22:06:58 +0200

    two_fer solved but unmentored

--- exercism/python/two-fer/two_fer.py
+++ exercism/python/two-fer/two_fer.py
@@ -1,7 +1,8 @@
-def two_fer(name):
+def two_fer(name=None):
     if name != None or name == 'you':
         return "One for " + name + ", one for me."
     elif name == None:
         return "One for you, one for me."
 
     pass
+print('running')
