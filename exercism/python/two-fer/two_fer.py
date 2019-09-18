c6efe5a5177660386dd481b6053b25d6fc3d799c
Christian Mondorf <christian@mondorf.dk>
2019-09-18 20:35:15 +0200

    Now on one line.

--- exercism/python/two-fer/two_fer.py
+++ exercism/python/two-fer/two_fer.py
@@ -1,8 +1,3 @@
 def two_fer(name=None):
-    if name != None or name == 'you':
-        return "One for " + name + ", one for me."
-    elif name == None:
-        return "One for you, one for me."
-
-    pass
-print('running')
+    return "One for " + name +", one for me." if name != None else "One for you, one for me."
+    
