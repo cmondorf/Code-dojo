7b5b1d334a0bcfdb24f140c00c77fbdcdfc9447c
Christian Mondorf <christian@mondorf.dk>
2019-12-26 11:39:41 +0100

    Solved bob.py from exercism.io

--- exercism/python/bob/bob.py
+++ exercism/python/bob/bob.py
@@ -1,11 +1,14 @@
 def hey(phrase):
 
-    if len(phrase.replace(" ", "")) == 0 or phrase.strip() == 0:
+    if len(phrase.replace(" ", "")) == 0 or len(phrase.strip()) == 0:
         return "Fine. Be that way!"
     elif phrase[-1] == "?" and phrase.isupper():
         return "Calm down, I know what I'm doing!"
-    elif phrase.strip()[-1] == "?":
-        return "Sure."
-    elif phrase.isupper():
+    try:
+        if phrase.strip()[-1] == "?":
+            return "Sure."
+    except:
+        pass
+    if phrase.isupper():
         return "Whoa, chill out!"
     return "Whatever."
